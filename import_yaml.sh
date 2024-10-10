47201261ee667d40fdd7bbbbc77207cfb925c933
NodEligible <dudarvasya1999@gmail.com>
2024-10-10 07:32:47 +0200

    Update import_yaml.sh

--- import_yaml.sh
+++ import_yaml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-def replace_port(docker_compose_file, old_port, new_port):
+import yaml
+
+def replace_port(docker_compose_file, 9005, 9003):
     # Читання файлу docker-compose.yml
     with open(docker_compose_file, 'r') as file:
         compose_content = yaml.safe_load(file)
@@ -11,22 +13,22 @@ def replace_port(docker_compose_file, old_port, new_port):
             # Перевірка кожного порту у списку портів
             for i, port_mapping in enumerate(service['ports']):
                 # Якщо порт у форматі "старий_порт:старий_порт"
-                if isinstance(port_mapping, str) and port_mapping.startswith(f"{old_port}:"):
+                if isinstance(port_mapping, str) and port_mapping.startswith(f"{9005}:"):
                     # Заміна порту
-                    service['ports'][i] = port_mapping.replace(str(old_port), str(new_port), 1)
+                    service['ports'][i] = port_mapping.replace(str(9005), str(9003), 1)
                 # Якщо порт у форматі словника (більш складний випадок)
-                elif isinstance(port_mapping, dict) and port_mapping.get('published') == old_port:
-                    service['ports'][i]['published'] = new_port
+                elif isinstance(port_mapping, dict) and port_mapping.get('published') == 9005:
+                    service['ports'][i]['published'] = 9003
 
     # Запис змін до файлу
     with open(docker_compose_file, 'w') as file:
         yaml.dump(compose_content, file, default_flow_style=False)
 
-    print(f"Порт {old_port} було змінено на {new_port} у {docker_compose_file}")
+    print(f"Порт {9005} було змінено на {9003} у {docker_compose_file}")
 
 # Використання скрипта
 docker_compose_file = 'docker-compose.yml'  # Вкажіть шлях до файлу
 old_port = 9005  # Порт, який потрібно замінити
 new_port = 9003  # Новий порт
 
-replace_port(docker_compose_file, old_port, new_port)
+replace_port(docker_compose_file, 9005, 9003)
