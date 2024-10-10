67121966b9aa3a04719bc13f44bd275a2464317c
NodEligible <dudarvasya1999@gmail.com>
2024-10-10 07:15:20 +0200

    Create import_yaml.sh

---
+++ import_yaml.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+import yaml
+
+def replace_port(docker_compose_file, old_port, new_port):
+    # Читання файлу docker-compose.yml
+    with open(docker_compose_file, 'r') as file:
+        compose_content = yaml.safe_load(file)
+
+    # Перевірка кожної служби
+    for service in compose_content.get('services', {}).values():
+        if 'ports' in service:
+            # Перевірка кожного порту у списку портів
+            for i, port_mapping in enumerate(service['ports']):
+                # Якщо порт у форматі "старий_порт:старий_порт"
+                if isinstance(port_mapping, str) and port_mapping.startswith(f"{old_port}:"):
+                    # Заміна порту
+                    service['ports'][i] = port_mapping.replace(str(old_port), str(new_port), 1)
+                # Якщо порт у форматі словника (більш складний випадок)
+                elif isinstance(port_mapping, dict) and port_mapping.get('published') == old_port:
+                    service['ports'][i]['published'] = new_port
+
+    # Запис змін до файлу
+    with open(docker_compose_file, 'w') as file:
+        yaml.dump(compose_content, file, default_flow_style=False)
+
+    print(f"Порт {old_port} було змінено на {new_port} у {docker_compose_file}")
+
+# Використання скрипта
+docker_compose_file = 'docker-compose.yml'  # Вкажіть шлях до файлу
+old_port = 9005  # Порт, який потрібно замінити
+new_port = 9003  # Новий порт
+
+replace_port(docker_compose_file, old_port, new_port)
