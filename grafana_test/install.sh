ba010a405917991073746509f84756ad3d5a6f8a
NodEligible <dudarvasya1999@gmail.com>
2024-11-21 16:28:00 +0100

    Update install.sh

--- grafana_test/install.sh
+++ grafana_test/install.sh
@@ -11,15 +11,7 @@ NC='\033[0m'
 
 echo -e "${YELLOW}Установка Grafana...${NC}"
 # Версія Grafana
-GRAFANA_VERSION="10.4.2"
-
-# Автоматичне отримання IP-адреси сервера
-PROMETHEUS_IP=$(hostname -I | awk '{print $1}')
-PROMETHEUS_URL="http://${PROMETHEUS_IP}:19980"
-
-echo "Автоматично визначена IP-адреса сервера: $PROMETHEUS_IP"
-echo "Prometheus URL: $PROMETHEUS_URL"
-
+GRAFANA_VERSION="6.4.3"
 # Встановлення необхідних залежностей
 apt-get install -y apt-transport-https software-properties-common wget curl
 mkdir -p /etc/apt/keyrings/
@@ -29,51 +21,18 @@ apt-get update
 apt-get install -y adduser libfontconfig1 musl
 
 # Завантаження та встановлення Grafana
-wget https://dl.grafana.com/oss/release/grafana_${GRAFANA_VERSION}_amd64.deb
-dpkg -i grafana_${GRAFANA_VERSION}_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_6.4.3_amd64.deb
+dpkg -i grafana_6.4.3_amd64.deb
 
 # Додавання Grafana до PATH
 echo "export PATH=/usr/share/grafana/bin:\$PATH" >> /etc/profile
-
-# Налаштування джерела даних Prometheus
-mkdir -p /etc/grafana/provisioning/datasources/
-cat <<EOF > /etc/grafana/provisioning/datasources/prometheus.yaml
-apiVersion: 1
-
-datasources:
-  - name: Prometheus
-    type: prometheus
-    url: ${PROMETHEUS_URL}
-EOF
-
-# Налаштування дашборда
-mkdir -p /etc/grafana/provisioning/dashboards/
-cat <<EOF > /etc/grafana/provisioning/dashboards/dashboard.yaml
-apiVersion: 1
-
-providers:
-  - name: 'Default'
-    orgId: 1
-    folder: ''
-    type: file
-    disableDeletion: false
-    editable: true
-    options:
-      path: /etc/grafana/dashboards
-EOF
-
-# Завантаження дашборда
-mkdir -p /etc/grafana/dashboards/
-curl -o /etc/grafana/dashboards/dashboard.json https://raw.githubusercontent.com/NodEligible/monitoring/refs/heads/main/dashboard/settings.json
 
 # Перезавантаження та запуск сервісу Grafana
 systemctl daemon-reload
 systemctl enable grafana-server
 systemctl start grafana-server
 
-# Виведення статусу Grafana
-systemctl status grafana-server
+rm -rf grafana_6.4.3_amd64.deb
 
 echo -e "${GREEN}Grafana установлена ​​с дашбордом!${NC}"
 echo -e "${YELLOW}Перейдите к Grafana, чтобы проверить дашборд по адресу: http://${PROMETHEUS_IP}:3000${NC}"
-echo -e "${YELLOW}Перейдите к Prometheus, чтобы проверить дашборд по адресу: http://${PROMETHEUS_IP}:19980${NC}"
