f424d41710177d6f3a728fc1fec4faddafe5a68a
NodEligible <dudarvasya1999@gmail.com>
2024-12-18 16:05:22 +0100

    Update healthcheck.sh

--- shardeum_healthcheck/healthcheck.sh
+++ shardeum_healthcheck/healthcheck.sh
@@ -1,31 +1,27 @@
-#!/bin/bash
+#! /bin/bash
 
 function get_status() {
-    docker exec -t shardeum-dashboard operator-cli status 2>/dev/null | grep "state" | awk '{ print $2 }'
+    STATUS=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }')
+    echo "${STATUS}"
 }
 
 cd "$HOME" || exit
 source .profile
 
-while true; do
-    printf "Check shardeum node status...\n"
+while true
+do
+    printf "Check shardeum node status \n"
     NODE_STATUS=$(get_status)
-
-    if [[ -z "${NODE_STATUS}" ]]; then
-        printf "Error: Unable to retrieve node status.\n"
-        sleep 1m
-        continue
-    fi
-
     printf "Current status: ${NODE_STATUS}\n"
-    if [[ "${NODE_STATUS}" == "stopped" ]]; then
-        printf "Node is stopped. Starting shardeum node and waiting for 5 minutes...\n"
-        docker exec -t shardeum-dashboard operator-cli start 2>/dev/null
+    sleep 5s
+    if [[ "${NODE_STATUS}" =~ "stopped" ]]; then
+        printf "Start shardeum node and wait 5 minutes\n"
+        docker exec -t shardeum-dashboard operator-cli start
         sleep 5m
     else
-        date=$(date +"%H:%M:%S")
-        printf "Node is running. Last Update: ${date}\n"
-        printf "Sleeping for 15 minutes...\n"
+        date=$(date +"%H:%M")
+        echo "Last Update: ${date}"
+        printf "Sleep 15 minutes\n"
         sleep 15m
     fi
 done
