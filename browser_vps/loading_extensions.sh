af1473518c08b4e2f03c9f72c56b45aaf7235ffc
NodEligible <dudarvasya1999@gmail.com>
2024-12-23 02:13:37 +0100

    Update loading_extensions.sh

--- browser_vps/loading_extensions.sh
+++ browser_vps/loading_extensions.sh
@@ -15,23 +15,24 @@ if [ ! -d "$EXT_DIR" ]; then
     mkdir -p "$EXT_DIR"
 fi
 
-# Запрашиваем ссылку на расширение
-read -p "Введите ссылку на расширение: " EXT_URL
+# Запрашиваем ID или ссылку
+read -p "Введите ID расширения или прямую ссылку: " INPUT
 
-# Проверяем, ввели ли ссылку
-if [ -z "$EXT_URL" ]; then
-    echo -e "${RED}Ссылка на расширение не введена. Попробуйте снова.${NC}"
-    exit 1
+# Проверяем, ссылка это или ID
+if [[ "$INPUT" =~ https?:// ]]; then
+    EXT_URL="$INPUT"
+else
+    EXT_URL="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=91.0.4472.124&acceptformat=crx3&x=id%3D$INPUT%26installsource%3Dondemand%26uc"
 fi
 
 # Генерация уникального ID для хранения расширения
-EXT_ID=$(basename "$EXT_URL" | cut -d'?' -f1 | md5sum | cut -d' ' -f1)
+EXT_ID=$(basename "$EXT_URL" | md5sum | cut -d' ' -f1)
 
 # Загружаем расширение
 echo -e "${YELLOW}Загружаем расширение из: $EXT_URL...${NC}"
 curl -L -o "$EXT_DIR/$EXT_ID.zip" "$EXT_URL"
 if [ $? -ne 0 ]; then
-    echo -e "${RED}Ошибка при загрузке расширения. Проверьте ссылку.${NC}"
+    echo -e "${RED}Ошибка при загрузке расширения. Проверьте ссылку или ID.${NC}"
     exit 1
 fi
 
@@ -48,7 +49,7 @@ if file "$EXT_DIR/$EXT_ID.zip" | grep -q "Zip archive data"; then
         exit 1
     fi
 else
-    echo -e "${RED}Файл не является корректным ZIP-архивом. Проверьте ссылку.${NC}"
+    echo -e "${RED}Файл не является корректным ZIP-архивом. Проверьте ссылку или ID.${NC}"
     rm "$EXT_DIR/$EXT_ID.zip"
     exit 1
 fi
