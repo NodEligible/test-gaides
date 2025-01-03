0cfd681df64187e2010404b22040b02155c2c189
NodEligible <dudarvasya1999@gmail.com>
2025-01-03 20:12:41 +0100

    Create install.sh

---
+++ OpenLedger/install.sh
@@ -0,0 +1,8 @@
+wget https://cdn.openledger.xyz/openledger-node-1.0.0-linux.zip
+
+sudo apt install unzip -y
+unzip openledger-node-1.0.0-linux.zip
+
+sudo apt-get install -f -y
+
+sudo apt-get install -y libgbm1
