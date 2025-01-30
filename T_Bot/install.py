716bec10925c80cabb1236626f0d8d76cbaecb63
NodEligible <dudarvasya1999@gmail.com>
2025-01-30 09:38:29 +0100

    Update install.py

--- T_Bot/install.py
+++ T_Bot/install.py
@@ -34,6 +34,16 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 import subprocess
 
+from dotenv import load_dotenv
+
+load_dotenv()  # Завантажує змінні з .env файлу
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+print("✅ BOT_TOKEN:", BOT_TOKEN)  # Додай для дебагу
+
+if not BOT_TOKEN:
+    raise ValueError("❌ Помилка: BOT_TOKEN не знайдено! Переконайтеся, що він є у .env файлі.")
+
 # === Налаштування бота ===
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID_FILE = "chat_id.txt"
