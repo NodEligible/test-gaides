cb4fd613eaa4b8d31b4c574cf18f7665e32db550
NodEligible <dudarvasya1999@gmail.com>
2025-03-04 14:37:45 +0100

    Update ritual_service.sh

--- container_service/ritual_service.sh
+++ container_service/ritual_service.sh
@@ -47,9 +47,9 @@ containers=("infernet-node" "deploy-fluentbit-1" "deploy-redis-1" "hello-world")
 while true; do
     restart_needed=false
 
-    for container in "${containers[@]}"; do
+    for container in "\${containers[@]}"; do
     current_time=$(date '+%Y-%m-%d %H:%M:%S')
-        if ! docker ps --format '{{.Names}}' | grep -q "^$container\$"; then
+        if ! docker ps --format '{{.Names}}' | grep -q "^\$container\$"; then
             echo -e "$current_time ${RED}⛔️ Контейнер $container не работает!${NC}" | tee -a "$LOG_FILE"
             restart_needed=true
         fi
