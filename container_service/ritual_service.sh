8d6d3745e38246f9772b54df15bfc1fa628fce11
NodEligible <dudarvasya1999@gmail.com>
2025-03-05 13:18:02 +0100

    Update ritual_service.sh

--- container_service/ritual_service.sh
+++ container_service/ritual_service.sh
@@ -56,13 +56,13 @@ while true; do
     if [ "\$restart_needed" = true ]; then
         docker compose -f "$COMPOSE_FILE" down
     echo -e "\$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') ⏳ ${YELLOW} Перезапускаем все контейнеры...${NC}" | tee -a "$LOG_FILE"   
-    sleep 20
+    sleep 1m
     docker compose -f "$COMPOSE_FILE" up -d
-    echo -e "\$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') 🔎 ${YELLOW} Контейнеры${NC} Ritual ${YELLOW}подняты, следующая проверка через 60 секунд.${NC}" | tee -a "$LOG_FILE"
+    echo -e "\$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') 🔎 ${YELLOW} Контейнеры${NC} Ritual ${YELLOW}подняты, следующая проверка через 20 минут.${NC}" | tee -a "$LOG_FILE"
     else
         echo -e "\$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') ✅ ${GREEN} Все контейнеры${NC} Ritual ${GREEN}работают корректно.${NC}" | tee -a "$LOG_FILE"
     fi
-    sleep 1m
+    sleep 20m
 done
 EOF
 
