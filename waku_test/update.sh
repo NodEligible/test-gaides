cb77486f9b892dea59d0a58e8ac50ea11c083a58
NodEligible <dudarvasya1999@gmail.com>
2025-07-30 05:28:37 +0200

    Update update.sh

--- waku_test/update.sh
+++ waku_test/update.sh
@@ -12,17 +12,17 @@ NC='\033[0m'
 cleanup() {
   echo -e "${YELLOW}Очистка...${NC}"
   docker-compose -f $HOME/nwaku-compose/docker-compose.yml down
-  # mkdir -p $HOME/nwaku_backups
-  # if [ -d "$HOME/nwaku_backups/keystore0.30" ]; then
-  #   echo "Бекап уже сделан"
-  # else
-  #   echo "Делаем бекап ключей"
-  #   mkdir -p $HOME/nwaku_backups/keystore0.30
-  #   cp $HOME/nwaku-compose/keystore/keystore.json $HOME/nwaku_backups/keystore0.30/keystore.json
-  #   rm -rf $HOME/nwaku-compose/keystore/
-  # fi
+  mkdir -p $HOME/nwaku_backups
+  if [ -d "$HOME/nwaku_backups/keystore0.34" ]; then
+    echo -e "${GREEN}Бекап уже сделан${NC}"
+  else
+    echo -e "${YELLOW}Делаем бекап ключей${NC}"
+    mkdir -p $HOME/nwaku_backups/keystore0.34
+    cp $HOME/nwaku-compose/keystore/keystore.json $HOME/nwaku_backups/keystore0.34/keystore.json
+    rm -rf $HOME/nwaku-compose/keystore
+  fi
   
-  # rm -rf $HOME/nwaku-compose/rln_tree/ 
+  rm -rf $HOME/nwaku-compose/rln_tree
   cd $HOME/nwaku-compose
   git restore . &>/dev/null
   echo -e "${GREEN}Чистка завершена!${NC}"
@@ -57,7 +57,7 @@ echo -e "${YELLOW}Обновление...${NC}"
   sed -i "s|RLN_RELAY_ETH_CLIENT_ADDRESS=.*|RLN_RELAY_ETH_CLIENT_ADDRESS=$RLN_RELAY_ETH_CLIENT_ADDRESS|" $HOME/nwaku-compose/.env
   sed -i "s|ETH_TESTNET_KEY=.*|ETH_TESTNET_KEY=$ETH_TESTNET_KEY|" $HOME/nwaku-compose/.env
   sed -i "s|RLN_RELAY_CRED_PASSWORD=.*|RLN_RELAY_CRED_PASSWORD=$RLN_RELAY_CRED_PASSWORD|" $HOME/nwaku-compose/.env
-  sed -i "s|NWAKU_IMAGE=.*|NWAKU_IMAGE=wakuorg/nwaku:v0.34.0|" $HOME/nwaku-compose/.env
+  sed -i "s|NWAKU_IMAGE=.*|NWAKU_IMAGE=wakuorg/nwaku:v0.35.1|" $HOME/nwaku-compose/.env
 
 
   # Меняем стандартный порт графаны, на случай если кто-то баловался с другими нодами 
@@ -65,7 +65,9 @@ echo -e "${YELLOW}Обновление...${NC}"
   sed -i 's/127\.0\.0\.1:4000:4000/0.0.0.0:4044:4000/g' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's/:5432:5432/:5444:5432/g' $HOME/nwaku-compose/docker-compose.yml
+  sed -i 's|- 80:80|- 8081:80|' $HOME/nwaku-compose/docker-compose.yml
 
+  bash $HOME/nwaku-compose/register_rln.sh
 }
 
 
@@ -84,7 +86,7 @@ echo_info() {
   echo -e "${YELLOW}   docker-compose -f $HOME/nwaku-compose/docker-compose.yml logs -f --tail=100 \n ${NC}"
   ip_address=$(hostname -I | awk '{print $1}') >/dev/null
   echo -e "${GREEN}Для проверки дашборда графаны, перейдите по ссылке: ${NC}"
-  echo -e "${YELLOW}   http://$ip_address:3004/d/yns_4vFVk/nwaku-monitoring \n ${NC}"
+  echo -e "${YELLOW}http://$ip_address:3004/d/yns_4vFVk/nwaku-monitoring \n ${NC}"
 }
 
 logo
