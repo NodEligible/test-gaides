95a212682b91c1719ef3f44c0e72ead7131ac009
NodEligible <dudarvasya1999@gmail.com>
2025-07-30 05:48:05 +0200

    Update update.sh

--- waku_test/update.sh
+++ waku_test/update.sh
@@ -13,12 +13,12 @@ cleanup() {
   echo -e "${YELLOW}Очистка...${NC}"
   docker-compose -f $HOME/nwaku-compose/docker-compose.yml down
   mkdir -p $HOME/nwaku_backups
-  if [ -d "$HOME/nwaku_backups/keystore0.34" ]; then
+  if [ -d "$HOME/nwaku_backups/keystore0.36.0" ]; then
     echo -e "${GREEN}Бекап уже сделан${NC}"
   else
     echo -e "${YELLOW}Делаем бекап ключей${NC}"
-    mkdir -p $HOME/nwaku_backups/keystore0.34
-    cp $HOME/nwaku-compose/keystore/keystore.json $HOME/nwaku_backups/keystore0.34/keystore.json
+    mkdir -p $HOME/nwaku_backups/keystore0.36.0
+    cp $HOME/nwaku-compose/keystore/keystore.json $HOME/nwaku_backups/keystore0.36.0/keystore.json
     rm -rf $HOME/nwaku-compose/keystore
   fi
   
@@ -30,6 +30,11 @@ cleanup() {
 
 update() {
 echo -e "${YELLOW}Обновление...${NC}"
+  # Иницифализируем
+  STORAGE_SIZE="50GB"
+  POSTGRES_SHM="5g"
+  ENV_FILE=$HOME/nwaku-compose/.env
+  KEYSTORE_PATH="$HOME/nwaku-compose/keystore/keystore.json"
   # Выгружаем переменные с .env в среду выполнения
   source $HOME/nwaku-compose/.env &>/dev/null
 
@@ -39,33 +44,55 @@ echo -e "${YELLOW}Обновление...${NC}"
   git pull origin master
   cp .env.example .env
 
-  if [ -z "$RLN_RELAY_ETH_CLIENT_ADDRESS" ]; then
-      echo -e "Введите ваш RPC Sepolia https url. Пример url'a - https://sepolia.infura.io/v3/ТУТ_ВАШ_КЛЮЧ"
-      read RLN_RELAY_ETH_CLIENT_ADDRESS
+  if grep -q "^STORAGE_SIZE=" "$ENV_FILE"; then
+      sed -i "s/^STORAGE_SIZE=.*/STORAGE_SIZE=$STORAGE_SIZE/" "$ENV_FILE"
+  else
+      echo "STORAGE_SIZE=$STORAGE_SIZE" >> "$ENV_FILE"
   fi
 
+  if grep -q "^POSTGRES_SHM=" "$ENV_FILE"; then
+      sed -i "s/^POSTGRES_SHM=.*/POSTGRES_SHM=$POSTGRES_SHM/" "$ENV_FILE"
+  else
+      echo "POSTGRES_SHM=$POSTGRES_SHM" >> "$ENV_FILE"
+  fi
+
+
+  #if [ -z "$RLN_RELAY_ETH_CLIENT_ADDRESS" ]; then
+      echo -e "${GREEN}Введите ваш RPC Linea Sepolia https url. Пример url'a - https://linea-sepolia.infura.io/v3/ТУТ_ВАШ_КЛЮЧ${NC}"
+      read RLN_RELAY_ETH_CLIENT_ADDRESS
+  #fi
+
+
+  if [ -z "$ETH_TESTNET_ACCOUNT" ]; then
+      echo -e "${GREEN}Введите ваш адрес ETH кошелька (начинается с 0х)${NC}"
+      read ETH_TESTNET_ACCOUNT
+  fi
+
+  
   if [ -z "$ETH_TESTNET_KEY" ]; then
-      echo -e "Введите ваш приватник от ETH кошелека на котором есть как минимум 0.1 ETH в сети Sepolia"
+      echo -e "${GREEN}Введите ваш приватник от ETH кошелька (без 0х)${NC}"
       read ETH_TESTNET_KEY
   fi
 
   if [ -z "$RLN_RELAY_CRED_PASSWORD" ]; then
-      echo -e "Введите(придумайте) пароль который будет использваться для сетапа ноды"
+      echo -e "${GREEN}Введите пароль который вводили в п.4 гайда${NC}"
       read RLN_RELAY_CRED_PASSWORD
   fi
 
   sed -i "s|RLN_RELAY_ETH_CLIENT_ADDRESS=.*|RLN_RELAY_ETH_CLIENT_ADDRESS=$RLN_RELAY_ETH_CLIENT_ADDRESS|" $HOME/nwaku-compose/.env
+  sed -i "s|ETH_TESTNET_ACCOUNT=.*|ETH_TESTNET_ACCOUNT=$ETH_TESTNET_ACCOUNT|" $HOME/nwaku-compose/.env
   sed -i "s|ETH_TESTNET_KEY=.*|ETH_TESTNET_KEY=$ETH_TESTNET_KEY|" $HOME/nwaku-compose/.env
   sed -i "s|RLN_RELAY_CRED_PASSWORD=.*|RLN_RELAY_CRED_PASSWORD=$RLN_RELAY_CRED_PASSWORD|" $HOME/nwaku-compose/.env
-  sed -i "s|NWAKU_IMAGE=.*|NWAKU_IMAGE=wakuorg/nwaku:v0.35.1|" $HOME/nwaku-compose/.env
+  sed -i "s|NWAKU_IMAGE=.*|NWAKU_IMAGE=wakuorg/nwaku:v0.36.0|" $HOME/nwaku-compose/.env
+  
 
-
-  # Меняем стандартный порт графаны, на случай если кто-то баловался с другими нодами 
+  # Меняем стандартный порт графаны
+  sed -i '/^version: "3.7"$/d' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's/0\.0\.0\.0:3000:3000/0.0.0.0:3004:3000/g' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's/127\.0\.0\.1:4000:4000/0.0.0.0:4044:4000/g' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
   sed -i 's/:5432:5432/:5444:5432/g' $HOME/nwaku-compose/docker-compose.yml
-  sed -i 's|- 80:80|- 8081:80|' $HOME/nwaku-compose/docker-compose.yml
+  sed -i 's/80:80/8081:80/g' $HOME/nwaku-compose/docker-compose.yml
 
   bash $HOME/nwaku-compose/register_rln.sh
 }
