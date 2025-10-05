4cab2ec99ba515e8ccbbc537ad172edb1b85e845
NodEligible <dudarvasya1999@gmail.com>
2025-10-05 23:41:48 +0200

    Update variant_2_install.sh

--- aztec/variant_2_install.sh
+++ aztec/variant_2_install.sh
@@ -74,7 +74,7 @@ RemainAfterExit=yes
 WorkingDirectory=$AZTEC_DIR
 ExecStart=/usr/bin/docker compose up -d
 ExecStop=/usr/bin/docker compose down
-TimeoutStartSec=0
+TimeoutStartSec=10
 
 [Install]
 WantedBy=multi-user.target
